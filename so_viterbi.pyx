6a155a031d6c2978180ce02fb06e7a0d6f41582a
Vamsi Krishna Surapureddi <vamsi@voicezen.ai>
2019-10-17 18:58:24 +0530

    Fixing for single words

--- so_viterbi.pyx
+++ so_viterbi.pyx
@@ -1,6 +1,6 @@
-#!/usr/bin/env python -*- coding: utf-8 -*- 
+#!/usr/bin/env python -*- coding: utf-8 -*-
 
-import cython 
+import cython
 import numpy as np
 import kenlm as klm
 
@@ -20,7 +20,7 @@ cdef class so_viterbi:
     def decode(self, list symbols, np.uint16_t T, np.uint16_t N):
         cdef object gen
         cdef bool missing, _missing, __missing
-        cdef unicode symbol_j, symbol_k, ngram 
+        cdef unicode symbol_j, symbol_k, ngram
         cdef np.ndarray[ndim=1, dtype=np.uint16_t, mode='c'] path
         cdef np.ndarray[ndim=3, dtype=np.float64_t, mode='c'] score
         cdef np.ndarray[ndim=3, dtype=np.uint16_t, mode='c'] backtrack
@@ -31,31 +31,34 @@ cdef class so_viterbi:
 
         #Initail unigram
         for i in range(N):
-            #if symbols[0][i] in self.model: 
+            #if symbols[0][i] in self.model:
             score[0, :,i] = next(self.model.full_scores(symbols[0][i]))[0]
             #else:
             #    score[0, :,i] = next(self.model.full_scores(symbols[0][i]))[0] + panelty
 
-        #Initail bigram
-        for j in range(N):
-            #missing = False
-            symbol_j =  symbols[0][j]
-            #if not symbol_j in self.model: 
-            #    missing = True
-            for k in range(N):
-                #_missing = False
-                #if not symbols[1][k] in self.model: 
-                #    _missing = True
-                ngram =  ' '.join((symbol_j, symbols[1][k]))
-                gen = self.model.full_scores(ngram)
-                gen_score = next(gen)[0]
-                gen_score = next(gen)[0]
-                #if missing:
-                #    gen_score += panelty    
-                #if _missing:
-                #    gen_score += panelty    
-                score[1, j, k] = gen_score + score[0, k, j]
-                #_missing = False
+        # if the string is of one word, we dont need bigram
+
+        if len(symbols) > 1:
+            #Initail bigram
+            for j in range(N):
+                #missing = False
+                symbol_j =  symbols[0][j]
+                #if not symbol_j in self.model:
+                #    missing = True
+                for k in range(N):
+                    #_missing = False
+                    #if not symbols[1][k] in self.model:
+                    #    _missing = True
+                    ngram =  ' '.join((symbol_j, symbols[1][k]))
+                    gen = self.model.full_scores(ngram)
+                    gen_score = next(gen)[0]
+                    gen_score = next(gen)[0]
+                    #if missing:
+                    #    gen_score += panelty
+                    #if _missing:
+                    #    gen_score += panelty
+                    score[1, j, k] = gen_score + score[0, k, j]
+                    #_missing = False
 
         #Recursive computation for 2 <= t <= T
         backtrack = np.zeros((T+1, N, N), dtype=np.uint16)
@@ -94,37 +97,38 @@ cdef class so_viterbi:
                     score[t, j, k] = max_val
                     backtrack[t, j, k] = max_ind
 
-        #Final bigram
-        for j in range(N):
-            #missing = False
-            max_ind = 0
-            max_val = NEGINF
-            symbol_j =  symbols[T-1][j]
-            #if not symbol_j in self.model:
-            #    missing = True
-            for i in range(N):
-                #_missing = False
-                #if not symbols[T-2][i] in self.model:
-                #    _missing = True
-                ngram =  ' '.join((symbols[T-2][i], symbol_j))
-                gen = self.model.full_scores(ngram)
-                gen_score = next(gen)[0]
-                gen_score = next(gen)[0]
-                gen_score = next(gen)[0]
-                #if missing:
-                #    gen_score += panelty
-                #if _missing:
-                #    gen_score += panelty
-                par_score = gen_score + score[T-1, i, j]
-                if par_score > max_val:
-                    max_ind = i
-                    max_val = par_score
-            score[T, j] = max_val
-            backtrack[T, j] = max_ind
+        if len(symbols) > 1:
+            #Final bigram
+            for j in range(N):
+                #missing = False
+                max_ind = 0
+                max_val = NEGINF
+                symbol_j =  symbols[T-1][j]
+                #if not symbol_j in self.model:
+                #    missing = True
+                for i in range(N):
+                    #_missing = False
+                    #if not symbols[T-2][i] in self.model:
+                    #    _missing = True
+                    ngram =  ' '.join((symbols[T-2][i], symbol_j))
+                    gen = self.model.full_scores(ngram)
+                    gen_score = next(gen)[0]
+                    gen_score = next(gen)[0]
+                    gen_score = next(gen)[0]
+                    #if missing:
+                    #    gen_score += panelty
+                    #if _missing:
+                    #    gen_score += panelty
+                    par_score = gen_score + score[T-1, i, j]
+                    if par_score > max_val:
+                        max_ind = i
+                        max_val = par_score
+                score[T, j] = max_val
+                backtrack[T, j] = max_ind
 
         #Backtrack
         path = np.empty(T+1, dtype=np.uint16)
-        PenUltimateState, UltimateState = np.unravel_index(np.argmax(score[T]),(N,N)) 
+        PenUltimateState, UltimateState = np.unravel_index(np.argmax(score[T]),(N,N))
         path[0] = UltimateState
         path[1] = PenUltimateState
         for i,t in enumerate(range(T-2, -1, -1)):
